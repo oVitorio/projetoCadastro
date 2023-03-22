800c059d7ec2a4a36e9dba10ba96787f536699d5
Vitório Cavalheiro <vitoriocavalheiro03@gmail.com>
2023-03-22 20:16:31 -0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projetoCadastro
+{
+    public partial class Form1 : Form
+    {
+
+   
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+           cadastro formsCadastro =  new cadastro();
+            string usuario = textBox1.Text;
+            string senha = textBox2.Text;
+            string mockUsuario = "admin";
+            string mockSenha = "1234";
+
+            if (usuario == mockUsuario &&  senha == mockSenha)
+            {
+                formsCadastro.Show();
+                this.Hide();
+            }
+            else
+            {
+            }
+
+
+        }
+    }
+}
